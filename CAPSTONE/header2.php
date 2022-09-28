e8ee1b0e5302b960690aa3399360870b299cfa46
karthnal <karthnal@gmail.com>
2022-09-28 14:40:49 +1000

    added username to welcome header

--- CAPSTONE/header2.php
+++ CAPSTONE/header2.php
@@ -89,7 +89,7 @@ if(isset($_POST['logout'])){
 	<!-- Button HTML (to Trigger Modal) -->
 	<!-- <a href="#myModal" class="trigger-btn" data-toggle="modal">Logout</a> -->
   <a href="../CAPSTONE/index.php" onclick="<?php session_destroy();?>" >Logout</a>
-  <p> Welcome, (Users name to be put here)</p>
+  <p> Welcome, <?php echo($_SESSION['uname']); ?></p>
  
 
 
